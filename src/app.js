b05c9b54eb14feb2eb184b0a5cf89c251fc8672b
Leonel Cedeño <52536063+Karel-CR@users.noreply.github.com>
2024-02-03 14:07:22 +0100

    Update app.js
    Cambios en la documentación

--- src/app.js
+++ src/app.js
@@ -13,11 +13,9 @@ window.onload = function() {
       for (let j = 0; j < adj.length; j++) {
         for (let k = 0; k < noun.length; k++) {
           for (let l = 0; l < domain.length; l++) {
+            //Generamos la cadena de texto.
             let completeDomain = pronoun[i] + adj[j] + noun[k]+'.' + domain[l];
-            // Inserta la combinación en el <span>
-            
-            
-
+            //Por cada "completeDomain" crearemos una etiqueta <p> en el html, de esta manera mostraremos los dominios uno por uno
             let p = document.createElement('p');
             p.textContent = completeDomain;
             elementDomain.appendChild(p);
