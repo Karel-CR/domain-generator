4a0f6dc8fb32b7a6b2fcd0d66b8dad611ab31fc1
Leonel Cedeño <52536063+Karel-CR@users.noreply.github.com>
2024-02-03 12:17:57 +0000

    Prueba

--- src/app.js
+++ src/app.js
@@ -1,30 +1,36 @@
-/* eslint-disable */
-/*
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-*/
-
 window.onload = function() {
   //declaramos arrays
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = ["before the class","right on time","when I finished","during my lunch","while I was praying"];
-  //Declaramos una función que se encargará de crear un  array con numeros aleatorios.
-  function generateRandomNumbers(who, action, what, when) {
-    let randomWho = parseInt(Math.random() * who.length);
-    let randomAction = parseInt(Math.random() * action.length);
-    let randomWhat = parseInt(Math.random() * what.length);
-    let randomWhen = parseInt(Math.random() * when.length);
-    return [randomWho, randomAction, randomWhat, randomWhen];
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = "com";
+  //Llamamos a la función correspondiente (Quiero que la funcion generateDomainCombinations() se cargué lo antes posible)
+  function generateDomainCombinations(pronoun, adj, noun, domain) {
+    let auxArray = [""];
+    let errorArray = [];
+    pronoun.forEach(element => {
+      if (auxArray[element] != pronoun) {
+        auxArray.push(pronoun[element]);
+      }
+      if (auxArray[element] != adj) {
+        auxArray[element + 1].push(adj[element]);
+        if (auxArray[element] != noun) {
+          auxArray[element + 2].push(noun[element]);
+        }
+        if (auxArray[element] != domain) {
+          auxArray[element + 3].push(domain[element]);
+        } else {
+          errorArray.push(1);
+        }
+        return auxArray;
+      }
+    });
+    console.log;
+    let arrayFinal = generateDomainCombinations(pronoun, adj, noun, domain);
+    var dominioElement = document.getElementById("dominio");
+    dominioElement.textContent = arrayFinal;
   }
   //Declaramos un Array para contener los números aleatorios
-  let arrayAleatoryNumbers = generateRandomNumbers(who, action, what, when);
   // Concatenamos cadenas
-  let myText = who[arrayAleatoryNumbers[0]]+" "+action[arrayAleatoryNumbers[1]]+" "+what[arrayAleatoryNumbers[2]]+" "+when[arrayAleatoryNumbers[3]];
-   // Imprimimos en el index
-  document.getElementById("excuse").innerHTML = myText;
+  // Imprimimos en el index
 };
